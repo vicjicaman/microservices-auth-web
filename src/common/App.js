1ef8ddf1f54283acc0ce7a1873e7154185e3d2a7
Victor Jimenez <vic.jicama@gmail.com>
2019-09-14 17:15:12 +0200

    Add shared layout component

--- src/common/App.js
+++ src/common/App.js
@@ -3,15 +3,14 @@ import Root from "Root";
 import { NavLink } from "react-router-dom";
 import { NavItem } from "reactstrap";
 import { Query } from "react-apollo";
-import * as Viewer from "Queries/viewer"
+import * as Viewer from "Queries/viewer";
 
-import Logout from 'Actions/auth/logout'
+import Logout from "Actions/auth/logout";
+import { Layout } from "@nebulario/microservice-layout";
 
 const App = () => {
   return (
-    <Query
-      query={Viewer.GET}
-    >
+    <Query query={Viewer.GET}>
       {({ loading, error, data }) => {
         if (loading) return <p>Loading...</p>;
         if (error) return <p>Error: {error}</p>;
@@ -20,50 +19,17 @@ const App = () => {
 
         return (
           <div>
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-              <a className="navbar-brand" href="#">
-                µservices
-              </a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarCollapse"
-                aria-controls="navbarCollapse"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarCollapse">
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item ">
-                    <a className="nav-link" href="#">
-                      Home
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="collapse navbar-collapse" id="navbarCollapse">
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <NavItem>
-                      {viewer.id === null ? (
-                        <NavLink className="nav-link" to="/auth">
-                          Login
-                        </NavLink>
-                      ) : (
-                        <Logout/>
-                      )}
-                    </NavItem>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-
-            <main role="main" className="container">
+            <Layout
+              viewer={viewer}
+              login={
+                <NavItem>
+                  <NavLink to="/auth">Login</NavLink>
+                </NavItem>
+              }
+              logout={<Logout />}
+            >
               <Root viewer={viewer} />
-            </main>
+            </Layout>
           </div>
         );
       }}
